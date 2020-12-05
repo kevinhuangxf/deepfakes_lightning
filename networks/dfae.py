92978caf05936e64ac525f9f29202aabdb52a661
Kevin Huang <apple163995@gmail.com>
2020-12-05 20:51:20 +0800

    update dfae

--- networks/dfae.py
+++ networks/dfae.py
@@ -21,14 +21,14 @@ class DeepFakesAutoEncoder(nn.Module):
             Conv2D_Transpose(512, 256),
             Conv2D_Transpose(256, 128),
             Conv2D_Transpose(128, 64),
-            nn.Conv2d(64, 3, kernel_size=5, padding=1),
+            nn.Conv2d(64, 3, kernel_size=5, padding=2),
             nn.Sigmoid(),
         )
         self.decoder_B = nn.Sequential(
-            _UpScale(512, 256),
-            _UpScale(256, 128),
-            _UpScale(128, 64),
-            nn.Conv2d(64, 3, kernel_size=5, padding=1),
+            Conv2D_Transpose(512, 256),
+            Conv2D_Transpose(256, 128),
+            Conv2D_Transpose(128, 64),
+            nn.Conv2d(64, 3, kernel_size=5, padding=2),
             nn.Sigmoid(),
         )
 
@@ -39,16 +39,17 @@ class DeepFakesAutoEncoder(nn.Module):
         else:
             out = self.encoder(x)
             out = self.decoder_B(out)
+        print(' shape : ', out.shape)
         return out
 
 
 class Conv2D(nn.Module):
 
-    def __init__(self, cin, cout, kernel_size=5, stride=2):
+    def __init__(self, cin, cout, kernel_size=5, stride=2, padding=2):
         super().__init__()
         self.conv_block = nn.Sequential(
-            nn.Conv2d(cin, cout, kernel_size, stride),
-            nn.BatchNorm2d(cout),
+            nn.Conv2d(cin, cout, kernel_size, stride, padding),
+            #nn.BatchNorm2d(cout),
             nn.LeakyReLU(0.1, inplace=True)
         )
 
@@ -58,11 +59,11 @@ class Conv2D(nn.Module):
 
 class Conv2D_Transpose(nn.Module):
 
-    def __init__(self, cin, cout, kernel_size=5, stride=2):
+    def __init__(self, cin, cout, kernel_size=5, stride=2, padding=2, output_padding=1):
         super().__init__()
         self.conv_block = nn.Sequential(
-            nn.ConvTranspose2d(cin, cout, kernel_size, stride),
-            nn.BatchNorm2d(cout),
+            nn.ConvTranspose2d(cin, cout, kernel_size, stride, padding, output_padding),
+            #nn.BatchNorm2d(cout),
             nn.LeakyReLU(0.1, inplace=True)
         )
 
