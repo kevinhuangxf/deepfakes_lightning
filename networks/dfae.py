36a26ed1eb2402663524da6fb97cb483b4413120
Kevin Huang <apple163995@gmail.com>
2020-12-07 00:42:21 +0800

    update dfae

--- networks/dfae.py
+++ networks/dfae.py
@@ -19,15 +19,21 @@ class DeepFakesAutoEncoder(nn.Module):
         )
         self.decoder_A = nn.Sequential(
             Conv2D_Transpose(512, 256),
+            ResidualBlock(256),
             Conv2D_Transpose(256, 128),
+            ResidualBlock(128),
             Conv2D_Transpose(128, 64),
+            ResidualBlock(64),
             nn.Conv2d(64, 3, kernel_size=5, padding=2),
             nn.Sigmoid(),
         )
         self.decoder_B = nn.Sequential(
             Conv2D_Transpose(512, 256),
+            ResidualBlock(256),
             Conv2D_Transpose(256, 128),
+            ResidualBlock(128),
             Conv2D_Transpose(128, 64),
+            ResidualBlock(64),
             nn.Conv2d(64, 3, kernel_size=5, padding=2),
             nn.Sigmoid(),
         )
@@ -54,6 +60,20 @@ class Reshape(nn.Module):
     def forward(self, input):
         output = input.view(-1, 1024, 16, 16)  # channel * 4 * 4
         return output
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, ch, kernel_size=5 ):
+        super().__init__()
+        self.conv1 = nn.Conv2d( ch, ch, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.conv2 = nn.Conv2d( ch, ch, kernel_size=kernel_size, padding=kernel_size // 2)
+
+    def forward(self, inp):
+        x = self.conv1(inp)
+        x = nn.LeakyReLU(0.2)(x)
+        x = self.conv2(x)
+        x = nn.LeakyReLU(0.2)(inp + x)
+        return x
 
 
 class _PixelShuffler(nn.Module):
